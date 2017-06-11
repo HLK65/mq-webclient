81ce19e0fde7db635ddf7dc901abbcf90d7d090a
Lukas HLK H <hlk65hlk@gmail.com>
2017-06-11 14:30:19 +0200

    stuff

--- js/main.js
+++ js/main.js
@@ -1,46 +1,62 @@
+//variable declaration
+var momAddress, momPort, userName, password, topicToday, topicWeek, topicAlert;
 var dev = false;
 //mom access data
 if (dev) {
-    var momAddress = "localhost";
-    var momPort = 15675;
-    var userName = "guest";
-    var password = "guest";
-    var topictoday = "test";
-    var topicweek = "test2";
-    var topicalert = "test3";
+    momAddress = "localhost";
+    momPort = 15675;
+    userName = "guest";
+    password = "guest";
+    topicToday = "test";
+    topicWeek = "test2";
+    topicAlert = "test3";
 } else {
-    var momAddress = "ec2-34-210-210-13.us-west-2.compute.amazonaws.com";
-    var momPort = 15675;
-    var userName = "weatherTenantOne:cadWebApp";
-    var password = "cadWebApp";
-    var topictoday = "78467/today/cep";
-    var topicweek = "78467/weekly/cep";
-    var topicalert = "78467/alert";
+    momAddress = "ec2-34-210-210-13.us-west-2.compute.amazonaws.com";
+    momPort = 15675;
+    userName = "weatherTenantOne:cadWebApp";
+    password = "cadWebApp";
+    topicToday = "78467/today";
+    topicWeek = "78467/weekly";
+    topicAlert = "78467/alert";
 }
 
 function changeCity(plz) {
     //TODO use cookie as storage
-    console.log("changeCity");
+    console.log("changeCity to " + plz);
+    topicToday = topicToday.replace(/[\d]*/, plz);
+    topicWeek = topicWeek.replace(/[\d]*/, plz);
+    topicAlert = topicAlert.replace(/[\d]*/, plz);
+
+    connect();
 }
 
-function login(a,b,c) {
+
+$("#loginForm").get(0).onsubmit = function login(event) {
     //TODO
-    console.log("login ",a,b,c);
-}
+    console.log("login ", event.toString(), $("#username").get(0).value, $("#password").get(0).value);
+
+    userName = $("#username").get(0).value;
+    password = $("#password").get(0).value;
+
+    connect();
+
+    event.preventDefault();
+    return false;
+};
 
 function getDateTime() {
     var datetime = new Date();
-    // var day = datetime.getDate().toString().length == 1 ? "0" + datetime.getDate() : datetime.getDate();
-    // var month = String(datetime.getMonth() + 1).length == 1 ? "0" + String(datetime.getMonth() + 1) : String(datetime.getMonth() + 1);
-    // var year = datetime.getFullYear().toString().length == 1 ? "0" + datetime.getFullYear() : datetime.getFullYear();
-    // var hour = datetime.getHours().toString().length == 1 ? "0" + datetime.getHours() : datetime.getHours();
-    // var min = datetime.getMinutes().toString().length == 1 ? "0" + datetime.getMinutes() : datetime.getMinutes();
-    //
-    // var string = day + "." + month + "." + year + " " + hour + ":" + min;
+    /*var day = datetime.getDate().toString().length == 1 ? "0" + datetime.getDate() : datetime.getDate();
+     var month = String(datetime.getMonth() + 1).length == 1 ? "0" + String(datetime.getMonth() + 1) : String(datetime.getMonth() + 1);
+     var year = datetime.getFullYear().toString().length == 1 ? "0" + datetime.getFullYear() : datetime.getFullYear();
+     var hour = datetime.getHours().toString().length == 1 ? "0" + datetime.getHours() : datetime.getHours();
+     var min = datetime.getMinutes().toString().length == 1 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+
+     var string = day + "." + month + "." + year + " " + hour + ":" + min;*/
     return datetime.toLocaleString();
 }
 
-setInterval(function() {
+setInterval(function () {
     $("#datetime").html(getDateTime());
 }, 250); //update every 250 ms
 
@@ -87,34 +103,41 @@ var chart = c3.generate({
 
 
 // Create a client instance
-var client = new Paho.MQTT.Client(momAddress, momPort, "/ws", userName);
+var client = new Paho.MQTT.Client(momAddress, momPort, "/ws", "weatherWebClient");
 
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
-// connect the client
-client.connect({userName: userName, password: password, onSuccess: onConnect});
 
+// connect the client
+function connect() {
+    // client.disconnect();
+    client.connect({userName: userName, password: password, onSuccess: onConnect, onFailure: onFailure});
+}
+
+// called when the client connection fails
+function onFailure(error) {
+    //TODO
+    console.log("onError", error.errorMessage);
+    alert("login failed");
+}
 
 // called when the client connects
 function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
+    // Once a connection has been made, subscribe to topics
     console.log("onConnect");
-    client.subscribe(topictoday);
-    client.subscribe(topicweek);
-    client.subscribe(topicalert);
-    //message = new Paho.MQTT.Message("Hello");
-    //message.destinationName = "test";
-    //client.send(message);
+    client.subscribe(topicToday);
+    client.subscribe(topicWeek);
+    client.subscribe(topicAlert);
 }
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
-    //reconnect TODO
     if (responseObject.errorCode !== 0) {
         console.log("onConnectionLost:" + responseObject.errorMessage);
     }
+    connect(); //try to reconnect
 }
 
 // called when a message arrives
@@ -124,231 +147,6 @@ function onMessageArrived(message) {
     //TODO parse, update html and canvas
 
 }
-
-
-/*
- switch (currentWeather){
- case 200:
- uebersetzt="Gewitter mit Leichtregen";
- break;
- case 201:
- uebersetzt="Gewitter mit Regen";
- break;
- case 202:
- uebersetzt="Gewitter mit Starkregen";
- break;
- case 210:
- uebersetzt="Leichtes Gewitter";
- break;
- case 211:
- uebersetzt="Gewitter";
- break;
- case 212:
- uebersetzt="Starkes Gewitter";
- break;
- case 221:
- uebersetzt="vereinzelt Schauer und Gewitter";
- break;
- case 230:
- uebersetzt="Gewitter mit Nieselregen";
- break;
- case 231:
- uebersetzt="Gewitter mit Regen";
- break;
- case 232:
- uebersetzt="Gewitter mit Starkregen";
- break;
- case 300:
- uebersetzt="Nieselregen";
- break;
- case 301:
- uebersetzt="Nieselregen";
- break;
- case 302:
- uebersetzt="Starker Nieselregen";
- break;
- case 310 :
- uebersetzt="Leichter Nieselregen";
- break;
- case 311:
- uebersetzt="Regenegen";
- break;
- case 312:
- uebersetzt="Starker SprÃ¼hregel";
- break;
- case 313:
- uebersetzt="Starkregen mit Nieselregen";
- break;
- case 314:
- uebersetzt="Starkregen";
- break;
- case 321:
- uebersetzt="andauernder Starkregen";
- break;
- case 500:
- uebersetzt="Leichter Regen";
- break;
- case 501:
- uebersetzt="moderater Regen";
- break;
- case 502:
- uebersetzt="Regen";
- break;
- case 503:
- uebersetzt="Extremer Starkregen";
- break;
- case 504:
- uebersetzt="extremer Regen";
- break;
- case 511:
- uebersetzt="gefrierender Regen";
- break;
- case 520:
- uebersetzt="Starkregen";
- break;
- case 521:
- uebersetzt="Starkregen";
- break;
- case 522:
- uebersetzt="extremer Starkregen";
- break;
- case 531:
- uebersetzt="Regenschauer";
- break;
- case 600:
- uebersetzt="Leichter Schneefall";
- break;
- case 601:
- uebersetzt="Schneefall";
- break;
- case 602:
- uebersetzt="Starker Schneefall";
- break;
- case 611:
- uebersetzt="Schneeregen";
- break;
- case 612:
- uebersetzt="Schneeregen";
- break;
- case 615:
- uebersetzt="Leichter Regen, Schneemix";
- break;
- case 616:
- uebersetzt="Regen, Schneemix";
- break;
- case 620:
- uebersetzt="leichter Schneefall";
- break;
- case 621:
- uebersetzt="Schneefall";
- break;
- case 622:
- uebersetzt="Starker Schneefall";
- break;
- case 701:
- uebersetzt="Nebel";
- break;
- case 711:
- uebersetzt="Dunst";
- break;
- case 721:
- uebersetzt="Dunstschleier";
- break;
- case 731:
- uebersetzt="Staub, Sand";
- break;
- case 741:
- uebersetzt="Nebel";
- break;
- case 751:
- uebersetzt="Sand";
- break;
- case 761:
- uebersetzt="Staub";
- break;
- case 762:
- uebersetzt="Vulkanasche";
- break;
- case 771:
- uebersetzt="SturmbÃ¶en";
- break;
- case 781:
- uebersetzt="Tornado";
- break;
- case 800:
- uebersetzt="Klarer Himmel";
- break;
- case 801:
- uebersetzt="Schleierwolken";
- break;
- case 802:
- uebersetzt="auflockernd BewÃ¶lkt";
- break;
- case 803:
- uebersetzt="abwechselnd BewÃ¶lkt";
- break;
- case 804:
- uebersetzt="BewÃ¶lkt";
- break;
- case 900:
- uebersetzt="Tornado";
- break;
- case 901:
- uebersetzt="Tropensturm";
- break;
- case 902:
- uebersetzt="Hurrikan";
- break;
- case 903:
- uebersetzt="kalt";
- break;
- case 904:
- uebersetzt="heiÃŸ";
- break;
- case 905:
- uebersetzt="windig";
- break;
- case 906:
- uebersetzt="Hagel";
- break;
- case 951:
- uebersetzt="windstill";
- break;
- case 952:
- uebersetzt="leichte Briese";
- break;
- case 953:
- uebersetzt="sanfte Briese";
- break;
- case 954:
- uebersetzt="moderate Briese";
- break;
- case 955:
- uebersetzt="frische Briese";
- break;
- case 956:
- uebersetzt="Starke Briese";
- break;
- case 957:
- uebersetzt="starker Wind";
- break;
- case 958:
- uebersetzt="leichter Wind";
- break;
- case 959:
- uebersetzt="Starkwind";
- break;
- case 960:
- uebersetzt="Sturm";
- break;
- case 961:
- uebersetzt="Starker Sturm";
- break;
- case 962:
- uebersetzt="Hurrikan";
- break;
- }
- */
 
 /*
  {
---
+++ js/weatherTranslation.js
@@ -0,0 +1,224 @@
+function getWeatherDesc(currentWeather) {
+    switch (currentWeather) {
+        case 200:
+            uebersetzt = "Gewitter mit Leichtregen";
+            break;
+        case 201:
+            uebersetzt = "Gewitter mit Regen";
+            break;
+        case 202:
+            uebersetzt = "Gewitter mit Starkregen";
+            break;
+        case 210:
+            uebersetzt = "Leichtes Gewitter";
+            break;
+        case 211:
+            uebersetzt = "Gewitter";
+            break;
+        case 212:
+            uebersetzt = "Starkes Gewitter";
+            break;
+        case 221:
+            uebersetzt = "vereinzelt Schauer und Gewitter";
+            break;
+        case 230:
+            uebersetzt = "Gewitter mit Nieselregen";
+            break;
+        case 231:
+            uebersetzt = "Gewitter mit Regen";
+            break;
+        case 232:
+            uebersetzt = "Gewitter mit Starkregen";
+            break;
+        case 300:
+            uebersetzt = "Nieselregen";
+            break;
+        case 301:
+            uebersetzt = "Nieselregen";
+            break;
+        case 302:
+            uebersetzt = "Starker Nieselregen";
+            break;
+        case 310 :
+            uebersetzt = "Leichter Nieselregen";
+            break;
+        case 311:
+            uebersetzt = "Regenegen";
+            break;
+        case 312:
+            uebersetzt = "Starker SprÃ¼hregel";
+            break;
+        case 313:
+            uebersetzt = "Starkregen mit Nieselregen";
+            break;
+        case 314:
+            uebersetzt = "Starkregen";
+            break;
+        case 321:
+            uebersetzt = "andauernder Starkregen";
+            break;
+        case 500:
+            uebersetzt = "Leichter Regen";
+            break;
+        case 501:
+            uebersetzt = "moderater Regen";
+            break;
+        case 502:
+            uebersetzt = "Regen";
+            break;
+        case 503:
+            uebersetzt = "Extremer Starkregen";
+            break;
+        case 504:
+            uebersetzt = "extremer Regen";
+            break;
+        case 511:
+            uebersetzt = "gefrierender Regen";
+            break;
+        case 520:
+            uebersetzt = "Starkregen";
+            break;
+        case 521:
+            uebersetzt = "Starkregen";
+            break;
+        case 522:
+            uebersetzt = "extremer Starkregen";
+            break;
+        case 531:
+            uebersetzt = "Regenschauer";
+            break;
+        case 600:
+            uebersetzt = "Leichter Schneefall";
+            break;
+        case 601:
+            uebersetzt = "Schneefall";
+            break;
+        case 602:
+            uebersetzt = "Starker Schneefall";
+            break;
+        case 611:
+            uebersetzt = "Schneeregen";
+            break;
+        case 612:
+            uebersetzt = "Schneeregen";
+            break;
+        case 615:
+            uebersetzt = "Leichter Regen, Schneemix";
+            break;
+        case 616:
+            uebersetzt = "Regen, Schneemix";
+            break;
+        case 620:
+            uebersetzt = "leichter Schneefall";
+            break;
+        case 621:
+            uebersetzt = "Schneefall";
+            break;
+        case 622:
+            uebersetzt = "Starker Schneefall";
+            break;
+        case 701:
+            uebersetzt = "Nebel";
+            break;
+        case 711:
+            uebersetzt = "Dunst";
+            break;
+        case 721:
+            uebersetzt = "Dunstschleier";
+            break;
+        case 731:
+            uebersetzt = "Staub, Sand";
+            break;
+        case 741:
+            uebersetzt = "Nebel";
+            break;
+        case 751:
+            uebersetzt = "Sand";
+            break;
+        case 761:
+            uebersetzt = "Staub";
+            break;
+        case 762:
+            uebersetzt = "Vulkanasche";
+            break;
+        case 771:
+            uebersetzt = "SturmbÃ¶en";
+            break;
+        case 781:
+            uebersetzt = "Tornado";
+            break;
+        case 800:
+            uebersetzt = "Klarer Himmel";
+            break;
+        case 801:
+            uebersetzt = "Schleierwolken";
+            break;
+        case 802:
+            uebersetzt = "auflockernd BewÃ¶lkt";
+            break;
+        case 803:
+            uebersetzt = "abwechselnd BewÃ¶lkt";
+            break;
+        case 804:
+            uebersetzt = "BewÃ¶lkt";
+            break;
+        case 900:
+            uebersetzt = "Tornado";
+            break;
+        case 901:
+            uebersetzt = "Tropensturm";
+            break;
+        case 902:
+            uebersetzt = "Hurrikan";
+            break;
+        case 903:
+            uebersetzt = "kalt";
+            break;
+        case 904:
+            uebersetzt = "heiÃŸ";
+            break;
+        case 905:
+            uebersetzt = "windig";
+            break;
+        case 906:
+            uebersetzt = "Hagel";
+            break;
+        case 951:
+            uebersetzt = "windstill";
+            break;
+        case 952:
+            uebersetzt = "leichte Briese";
+            break;
+        case 953:
+            uebersetzt = "sanfte Briese";
+            break;
+        case 954:
+            uebersetzt = "moderate Briese";
+            break;
+        case 955:
+            uebersetzt = "frische Briese";
+            break;
+        case 956:
+            uebersetzt = "Starke Briese";
+            break;
+        case 957:
+            uebersetzt = "starker Wind";
+            break;
+        case 958:
+            uebersetzt = "leichter Wind";
+            break;
+        case 959:
+            uebersetzt = "Starkwind";
+            break;
+        case 960:
+            uebersetzt = "Sturm";
+            break;
+        case 961:
+            uebersetzt = "Starker Sturm";
+            break;
+        case 962:
+            uebersetzt = "Hurrikan";
+            break;
+    }
+    return uebersetzt;
+}
